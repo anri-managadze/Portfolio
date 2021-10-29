b918992c8fc20d0cdbc4cc633a954ef64d47e2f9
anri-managadze <anrimanagadze@gmail.com>
2021-10-29 17:41:14 +0400

    commit

--- src/script.js
+++ src/script.js
@@ -1,8 +1,25 @@
 
+// Change color (darkmode/lightmode) start
 
+const colorChangeBtn = document.querySelector('.change');
 
-document.querySelector('.change').addEventListener('click',myReload);
-function myReload() {
-    document.querySelector(".ggg").classList.toggle("kkk");
+colorChangeBtn.addEventListener("click", function () {
+    document.querySelector(".light-mode").classList.toggle("dark-mode");
 
+    let theme = "light";
+    if (document.querySelector(".light-mode").classList.contains("dark-mode")) {
+        theme = "dark";
+    }
+    localStorage.setItem("theme", theme);
+});
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme === "dark") {
+    document.querySelector(".light-mode").classList.add("dark-mode");
 }
+
+// Change color (darkmode/lightmode) end
+
+document.querySelector('.reload').addEventListener('click', function (){
+    location.reload();
+})
